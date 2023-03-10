c1339fd7f5f67553aef4c69f9089cec3eda84e76
Tomas Maly <malytomas@ucpu.cz>
2023-03-10 19:44:22 +0100

    costs, separator, tokens

--- public/common.js
+++ public/common.js
@@ -1,50 +1,60 @@
 
 function makeClasses(d) {
-	let arr = []
+	let arr = [];
 	for (let t of d.tgs)
-		arr.push("tag-" + t)
-	arr.push("card")
-	return arr.join(" ")
+		arr.push("tag-" + t);
+	arr.push("card");
+	return arr.join(" ");
 }
 
 function makeImgSrc(d) {
-	return "images/" + d.id + ".png"
+	return "images/" + d.id + ".png";
 }
 
 function makeTags(d) {
-	return d.tgs.join(", ")
+	return d.tgs.join(", ");
+}
+
+function replaceImages(str) {
+	return str
+		.replace(/\\M/g, "<img src=\"media/military.png\" alt=\"M\">")
+		.replace(/\\C/g, "<img src=\"media/culture.png\" alt=\"C\">")
+		.replace(/\\F/g, "<img src=\"media/faith.png\" alt=\"F\">")
+		.replace(/\$G/g, "<img src=\"media/gold.png\" alt=\"G\">")
+		.replace(/\$M/g, "<img src=\"media/mana.png\" alt=\"M\">")
+		.replace(/\$F/g, "<img src=\"media/fire.png\" alt=\"F\">");
+}
+
+function makeCosts(d) {
+	let arr = [];
+	for (let a of d.Cost.split(";"))
+		arr.push(a.trim());
+	return replaceImages(arr.join("<br>"));
 }
 
 function makeStats(d) {
-	let arr = []
-	if (d.Culture.length > 0)
-		arr.push(d.Culture + "<img src=\"media/culture.png\">")
+	let arr = [];
 	if (d.Military.length > 0)
-		arr.push(d.Military + "<img src=\"media/military.png\">")
+		arr.push(d.Military + " \\M");
+	if (d.Culture.length > 0)
+		arr.push(d.Culture + " \\C");
 	if (d.Faith.length > 0)
-		arr.push(d.Faith + "<img src=\"media/faith.png\">")
-	return "&nbsp;" + arr.join("&nbsp;&nbsp;") + "&nbsp;"
+		arr.push(d.Faith + " \\F");
+	return replaceImages("&nbsp;" + arr.join(" &nbsp; ") + "&nbsp;");
 }
 
 function makeDescription(d) {
-	let arr = []
-	for (let a of d.Description.split(";")) {
-		arr.push(a.trim()
-			.replace(/^'/, "")
-			.replace(/\\C/g, "<img src=\"media/culture.png\">")
-			.replace(/\\M/g, "<img src=\"media/military.png\">")
-			.replace(/\\F/g, "<img src=\"media/faith.png\">")
-		);
-	}
-	return arr.join("<br>");
+	let arr = [];
+	for (let a of d.Description.split(";"))
+		arr.push(a.trim().replace(/^'/, ""));
+	return replaceImages(arr.join("<br>"));
 }
 
 function makeLore(d) {
-	let arr = []
-	for (let a of d.Lore.split(";")) {
+	let arr = [];
+	for (let a of d.Lore.split(";"))
 		arr.push(a.trim());
-	}
-	return arr.join("<br>");
+	return replaceImages(arr.join("<br>"));
 }
 
 function findCard(data, id) {
@@ -68,9 +78,25 @@ function updateCards(data) {
 			.attr("src", makeImgSrc)
 			.attr("alt", makeImgSrc);
 
+		let box = cont.append("div")
+			.classed("box", true);
+
+		cont.append("div")
+			.classed("separator", true)
+			.append("img")
+			.attr("src", "media/separator.png");
+
 		cont.append("div")
 			.classed("name", true)
 			.text(d => d.Name);
+
+		let costs = makeCosts(d);
+		if (costs.indexOf("img") > 0)
+		{
+			cont.append("div")
+				.classed("costs", true)
+				.html(costs);
+		}
 
 		let stats = makeStats(d);
 		if (stats.indexOf("img") > 0)
@@ -80,12 +106,12 @@ function updateCards(data) {
 				.html(stats);
 		}
 
-		let box = cont.append("div").classed("box", true);
 		if (d.Description.length > 0) {
 			box.append("div")
 				.classed("description", true)
 				.html(makeDescription);
 		}
+
 		if (d.Lore.length > 0) {
 			box.append("div")
 				.classed("lore", true)
